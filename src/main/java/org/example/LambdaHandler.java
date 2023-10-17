336f5b5459f07c435d601a6c9cd385caee01f4c6
Juan Pablo Agudelo <juapaagu@bancolombia.com.co>
2023-10-17 01:18:34 -0500

    Se agregan nuevos logs a la funcion lambda

--- src/main/java/org/example/LambdaHandler.java
+++ src/main/java/org/example/LambdaHandler.java
@@ -5,9 +5,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class LambdaHandler implements RequestHandler<String, String> {
     @Override
-    public String handleRequest(String Mes, Context context) {
-
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Version 2.0");
         context.getLogger().log(">>>>>>>> Funcion lambda ejecutada");
+        context.getLogger().log(">>>>>>>> Mensaje enviado: " + input);
         return "DONE";
     }
 }
